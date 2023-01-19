71da5660f336cec2cfd492459a5138d7b62d8978
recorror <itgwd9701@gmail.com>
2023-01-19 13:28:12 +0900

    Modify : appjs 라우터 추가

--- front_Nanal/src/App.js
+++ front_Nanal/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './another/Nav.js'
+import Calendar from './main/Calendar.js'
+import SignIn from './account/SignIn.js'
+import NotFound from './NotFound.js'
 
 function App() {
+  const isLogin = true
+
   return (
     <div className="App">
+      <Nav/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Calendar/>}></Route>
+          <Route path="/youshallnotpass" element={<SignIn/>}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
