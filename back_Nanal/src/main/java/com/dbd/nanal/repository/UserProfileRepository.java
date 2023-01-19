a5f757b09a31f127b7ef0fca9457f28a0c48e230
LimYujung <melimelame@gmail.com>
2023-01-19 17:35:07 +0900

    Feat: User Entity 추가

---
+++ back_Nanal/src/main/java/com/dbd/nanal/repository/UserProfileRepository.java
@@ -0,0 +1,11 @@
+package com.dbd.nanal.repository;
+
+import com.dbd.nanal.model.UserProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long> {
+
+
+}
