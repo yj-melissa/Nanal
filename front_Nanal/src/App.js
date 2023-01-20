6e5994bed139b3e22e5e6b301ba349fd8b371261
recorror <itgwd9701@gmail.com>
2023-01-20 12:44:49 +0900

    Modify : nav3 + 로그인 회원가입 form
    nav 기본 폼 맞추기 => 로그인 회원가입 기본 form만 맞춰놓음.

--- front_Nanal/src/App.js
+++ front_Nanal/src/App.js
@@ -5,6 +5,7 @@ import Nav from "./another/Nav.js";
 import Calendar from "./main/Calendar.js";
 import BookCase from "./main/BookCase.js";
 import SignIn from "./account/SignIn.js";
+import SignUp from "./account/SignUp.js";
 import NotFound from "./NotFound.js";
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
         ) : (
           <Route path="/" element={<BookCase />}></Route>
         )}
-
+        <Route path="/SignUp" element={<SignUp />}></Route>
         <Route path="/youshallnotpass" element={<SignIn />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
