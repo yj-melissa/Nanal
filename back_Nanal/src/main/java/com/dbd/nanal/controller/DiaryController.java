a9e48b8e2c2457fea153315a7d3cdb1552d521d7
sohui <psh990525@naver.com>
2023-01-23 01:52:10 +0900

    Feat : Diary Controller get, delete 작성

--- back_Nanal/src/main/java/com/dbd/nanal/controller/DiaryController.java
+++ back_Nanal/src/main/java/com/dbd/nanal/controller/DiaryController.java
@@ -12,8 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
-@Api(tags = {"Diary"})
+@Api(tags = {"Diary관련 API"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("diary")
@@ -23,41 +24,66 @@ public class DiaryController {
 
     @ApiOperation(value = "일기 생성", notes = "새로운 일기를 생성합니다.")
     @PostMapping("")
-    public ResponseEntity<?> writeDiary(@ApiParam(value = "일기 정보")@RequestBody DiaryRequestDTO diary, HttpSession session) throws Exception{
+    public ResponseEntity<?> writeDiary(@ApiParam(value = "일기 정보")@RequestBody DiaryRequestDTO diary, HttpSession session) {
         try{
             //diary keyword analyze
             //picture
             //music
             return new ResponseEntity<>(diaryService.save(diary.toEntity()), HttpStatus.OK);
         }catch (Exception e){
-            return new ResponseEntity<String>("서버오류", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("서버오류", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @ApiOperation(value = "일기 반환", notes = "일기를 반환합니다.")
+    @ApiOperation(value = "diaryIdx로 일기 내용 조회", notes =
+            "일기 내용을 조회합니다.\n" +
+                    "[Front] \n" +
+                    "{diaryIdx(int)} \n\n" +
+                    "[Back] \n" +
+                    "{diaryIdx(int), userIdx(int), creationDate(Date), content(String), picture(String), music(int), emo(String)} ")
     @GetMapping("/{diaryIdx}")
-    public ResponseEntity<?> getDiary(@ApiParam(value = "그룹 id")@PathVariable("diaryIdx") int diaryIdx)throws  Exception{
+    // 일기 리턴
+    public ResponseEntity<?> getDiary(@ApiParam(value = "일기 id")@PathVariable("diaryIdx") int diaryIdx){
         try{
-            DiaryResponseDTO dto=diaryService.getDiary(diaryIdx);
-            if(dto!=null){
-                return new ResponseEntity<DiaryResponseDTO>(dto, HttpStatus.OK);
-            }else{
+            DiaryResponseDTO diaryResponseDTO=diaryService.getDiary(diaryIdx);
+            if(diaryResponseDTO!=null)
+                return new ResponseEntity<>(diaryResponseDTO, HttpStatus.OK);
+            else
                 return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-            }
-
         }catch (Exception e){
-            return new ResponseEntity<String>("서버오류", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("서버오류", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-//    @ApiOperation(value = "일기 리스트 반환", notes = "일기 리스트를 반환합니다.")
-//    @GetMapping("/{groupId}")
-//    public ResponseEntity<?> DiaryList(@PathVariable("groupId") int groupId)throws  Exception{
+    @ApiOperation(value = "diaryIdx로 일기 삭제", notes =
+            "일기를 삭제합니다.\n" +
+                    "[Front] \n" +
+                    "{diaryIdx(int)} \n\n" +
+                    "[Back] \n" +
+                    "ok(200)")
+    @DeleteMapping("/{diaryIdx}")
+    public ResponseEntity<?> deleteDiary(@ApiParam(value="일기 id") @PathVariable("diaryIdx") int diaryIdx){
+        try{
+            diaryService.deleteDiary(diaryIdx);
+            return new ResponseEntity<>("삭제 완료", HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>("서버오류", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+//    @ApiOperation(value = "groupIdx로 일기 리스트 리턴", notes =
+//            "일기를 삭제합니다.\n" +
+//                    "[Front] \n" +
+//                    "{groupIdx(int)} \n\n" +
+//                    "[Back] \n" +
+//                    "[{diaryIdx(int), userIdx(int), creationDate(Date), content(String), picture(String), music(int), emo(String)}]")
+//    @GetMapping("/list/{groupIdx}")
+//    public ResponseEntity<?> DiaryList(@PathVariable("groupIdx") int groupIdx){
 //        try{
-//            return new ResponseEntity<List<DiaryResponseDTO>>(diaryService.diaryList(groupId));
+//            List<DiaryResponseDTO> diaryRequestDTOList=diaryService.diaryList(groupIdx);
+//            return ResponseEntity.status(HttpStatus.OK).body(diaryRequestDTOList);
 //        }catch (Exception e){
-//            return new ResponseEntity<String>("서버오류", HttpStatus.INTERNAL_SERVER_ERROR);
+//            return new ResponseEntity<>("서버오류", HttpStatus.INTERNAL_SERVER_ERROR);
 //        }
 //    }
-
 }
--- back_Nanal/src/main/java/com/dbd/nanal/dto/DiaryResponseDTO.java
+++ back_Nanal/src/main/java/com/dbd/nanal/dto/DiaryResponseDTO.java
@@ -1,33 +1,32 @@
 package com.dbd.nanal.dto;
 
 import com.dbd.nanal.model.DiaryEntity;
-import lombok.Data;
+import lombok.Getter;
 
 import java.util.Date;
 
-@Data
+@Getter
 public class DiaryResponseDTO {
     private int diaryIdx;
     private int userIdx;
     private Date creationDate;
     private String content;
-    private int picture;
+    private String picture;
     private int music;
-    private boolean isDeleted;
-    private Date deleteDate;
-    private Date expireDate;
+    //    private boolean isDeleted;
+//    private Date deleteDate;
+//    private Date expireDate;
     private String emo;
+
 
     public DiaryResponseDTO(DiaryEntity diary) {
         this.diaryIdx = diary.getDiaryIdx();
-        this.userIdx = diary.getUser().getUserIdx();
-        this.creationDate = diary.getCreation_date();
+//        this.userIdx = diary.getUser().getUserIdx();
+        this.creationDate = diary.getCreationDate();
         this.content = diary.getContent();
-        this.picture = diary.getPainting().getPictureIdx();
-        this.music = diary.getMusic().getMusicIdx();
-        this.isDeleted = diary.isDeleted();
-        this.deleteDate = diary.getDeleteDate();
-        this.expireDate = diary.getExpireDate();
+//        this.picture = diary.getPainting().getPicturePath();
+//        this.music = diary.getMusic().getMusicIdx();
         this.emo = diary.getEmo();
     }
+
 }
