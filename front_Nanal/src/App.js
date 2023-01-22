e554a1a2cc623224189d9bd5963a34b9419cd7aa
Awake-Ukey <hn99515@gmail.com>
2023-01-23 01:33:37 +0900

    Feat : Diary Create

--- front_Nanal/src/App.js
+++ front_Nanal/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { useState, useReducer, useRef } from "react";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./another/Nav.js";
 import Calendar from "./main/Calendar.js";
@@ -7,9 +7,64 @@ import BookCase from "./main/BookCase.js";
 import SignIn from "./account/SignIn.js";
 import SignUp from "./account/SignUp.js";
 import NotFound from "./NotFound.js";
+import DiaryCreate from "./diary/DiaryCreate";
+import DiaryList from "./diary/DiaryList";
+import DiaryEditor from "./components/DiaryEditor";
+
+// 더미 데이터
+// const dummyList = [
+//   {
+//     id: 1,
+//     date: "",
+//     content: "일기 1",
+//     group: "private",
+//     // 날짜는 숫자로 변환해서 받기
+//     created_at: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     date: "",
+//     content: "일기 2",
+//     group: "group",
+//     // 날짜는 숫자로 변환해서 받기
+//     created_at: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     date: "",
+//     content: "일기 3",
+//     group: "private",
+//     // 날짜는 숫자로 변환해서 받기
+//     created_at: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     date: "",
+//     content: "일기 4",
+//     group: "group",
+//     // 날짜는 숫자로 변환해서 받기
+//     created_at: new Date().getTime(),
+//   },
+// ];
 
 function App() {
   const isCalendar = true;
+  // 일기 데이터는 부모 컴포넌트에서 관리 (데이터는 단방향으로 흐르기 떄문)
+  const [data, setData] = useState([]);
+  // id는 useRef로 생성
+  const dataId = useRef(0);
+  const onCreate = (content, group) => {
+    const created_at = new Date().getTime();
+    const newItem = {
+      id: dataId.current,
+      content,
+      group,
+      created_at,
+    };
+    dataId.current += 1;
+    // 새로운 일기를 가장 위로
+    setData([newItem, ...data]);
+  };
 
   return (
     <div className="App">
@@ -22,8 +77,11 @@ function App() {
         )}
         <Route path="/SignUp" element={<SignUp />}></Route>
         <Route path="/SignIn" element={<SignIn />}></Route>
+        <Route path="/New" element={<DiaryCreate />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
--- front_Nanal/src/diary/DiaryCreate.js
+++ front_Nanal/src/diary/DiaryCreate.js
@@ -1,5 +1,12 @@
+import DiaryEditor from "../components/DiaryEditor";
+
 function DiaryCreate() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>오늘의 일기</h2>
+      <DiaryEditor />
+    </div>
+  );
 }
 
 export default DiaryCreate;
