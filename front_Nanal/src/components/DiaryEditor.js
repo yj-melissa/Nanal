9997ad19cfa1c31c9d77d73330be20e454641b24
Awake-Ukey <hn99515@gmail.com>
2023-01-24 00:39:33 +0900

    Modify : diary CRUD 최적화

--- front_Nanal/src/components/DiaryEditor.js
+++ front_Nanal/src/components/DiaryEditor.js
@@ -1,5 +1,5 @@
 // 일기 작성, 수정 부분에서 사용할 컴포넌트
-import { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 // 오늘 날짜를 YYYY-MM-DDTHH:mm:ss 형태로 변환해주는 함수
 const getStringDate = (date) => {
@@ -79,4 +79,5 @@ function DiaryEditor({ onCreate }) {
   );
 }
 
-export default DiaryEditor;
+//
+export default React.memo(DiaryEditor);
