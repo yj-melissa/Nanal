16f83b9b112c2426671055f3706bc73ac12bf9b2
박소희 <psh990525@naver.com>
2023-01-25 14:44:50 +0900

    Modify : Diary create, getDiaryList query 수정

--- back_Nanal/src/main/java/com/dbd/nanal/repository/DiaryRepository.java
+++ back_Nanal/src/main/java/com/dbd/nanal/repository/DiaryRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<DiaryEntity, Integer> {
-    @Query("select a from  DiaryEntity a join GroupDiaryRelationEntity b on  b.diary.diaryIdx=a.diaryIdx where b.groupDetail.groupIdx=:groupIdx")
+    @Query("select a from  DiaryEntity a join GroupDiaryRelationEntity b on  b.diary.diaryIdx=a.diaryIdx where b.groupDetail.groupIdx=:groupIdx and a.isDeleted=false")
     List<DiaryEntity> findGroupDiaryList(@Param("groupIdx") int groupIdx);
 
 
