771fe9c9b48984c8824e485b3724eea5dc7b040a
LimYujung <lyjung0704@gmail.com>
2023-01-25 17:50:03 +0900

    Modify : 회원가입, 로그인 예외처리 수정

--- back_Nanal/src/main/java/com/dbd/nanal/service/UserService.java
+++ back_Nanal/src/main/java/com/dbd/nanal/service/UserService.java
@@ -6,6 +6,7 @@ import com.dbd.nanal.repository.UserProfileRepository;
 import com.dbd.nanal.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,23 +18,25 @@ public class UserService {
     @Autowired private final UserProfileRepository userProfileRepository;
 
     public UserEntity join(final UserEntity userEntity, final UserProfileEntity userProfileEntity) {
-        if(userEntity == null || userProfileEntity == null || userEntity.getUserId() == null || userEntity.getEmail() == null || userProfileEntity.getNickname() == null) {
-            throw new RuntimeException("필수항목이 입력되었는지 확인하세요");
+        try {
+//            userEntity == null || userProfileEntity == null || userEntity.getUserId() == null || userEntity.getEmail() == null || userProfileEntity.getNickname() == null;
+        } catch {
+            throw new NullPointerException("");
         }
 
         final String userId = userEntity.getUserId();
         if(userRepository.existsByUserId(userId)) {
-            throw new RuntimeException("이미 사용중인 아이디입니다");
+            throw new DuplicateKeyException(userId);
         }
 
         final String email = userEntity.getEmail();
         if(userRepository.existsByEmail(email)) {
-            throw new RuntimeException("이미 사용중인 이메일입니다");
+            throw new DuplicateKeyException(email);
         }
 
         final String nickname = userProfileEntity.getNickname();
         if(userProfileRepository.existsByNickname(nickname)) {
-            throw new RuntimeException("이미 사용중인 닉네임입니다");
+            throw new DuplicateKeyException(nickname);
         }
 
         UserEntity newUser = userRepository.save(userEntity);
