b70464c94def4ce6e9bf57ce956cfce09d6924ad
LimYujung <melimelame@gmail.com>
2023-01-25 13:08:35 +0900

    Modify : UserController CORS 처리, UserRepository 자료형 수정

--- back_Nanal/src/main/java/com/dbd/nanal/config/WebConfig.java
+++ back_Nanal/src/main/java/com/dbd/nanal/config/WebConfig.java
@@ -1,4 +1,19 @@
 package com.dbd.nanal.config;
 
-public class WebConfig {
-}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//            .allowedOrigins("http://192.168.100.94:3030")
+//            .allowedMethods("*")
+//            .allowCredentials(false)
+//            .maxAge(3000);
+//    }
+//}
--- back_Nanal/src/main/java/com/dbd/nanal/controller/UserController.java
+++ back_Nanal/src/main/java/com/dbd/nanal/controller/UserController.java
@@ -14,10 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -30,7 +33,8 @@ public class UserController {
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@Valid UserDTO userDTO) {
+    public ResponseEntity<?> signUp(@RequestBody UserDTO userDTO) {
+        System.out.println("signUp : "+userDTO.getUserId()+userDTO.getUserPassword());
         try {
             if (userDTO == null || userDTO.getUserPassword() == null) {
                 throw new RuntimeException("비밀번호 값이 유효하지 않습니다");
@@ -90,6 +94,7 @@ public class UserController {
                 .jwtToken(token)
                 .build();
             return ResponseEntity.ok().body(responseUserDTO);
+
         } else {
             String errorMsg;
             Boolean isUserExist = userService.findUserId(userDTO.getUserId());
