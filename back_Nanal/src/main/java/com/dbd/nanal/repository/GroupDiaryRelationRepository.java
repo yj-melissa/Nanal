220d9629b42cf945ac95007cd051f1f9ab050e6e
박소희 <psh990525@naver.com>
2023-01-25 10:59:16 +0900

    Feat : Diary 작성시 GroupDiary 저장

---
+++ back_Nanal/src/main/java/com/dbd/nanal/repository/GroupDiaryRelationRepository.java
@@ -0,0 +1,7 @@
+package com.dbd.nanal.repository;
+
+import com.dbd.nanal.model.GroupDiaryRelationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupDiaryRelationRepository extends JpaRepository<GroupDiaryRelationEntity, Integer> {
+}
