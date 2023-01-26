efe2a0848193acd770a7cb25b60da4f421962867
박소연 <soyeon_9605@yu.ac.kr>
2023-01-26 15:17:26 +0900

    Modify : login 수정
    - 패키지 삭제
    - 회원가입 일부 수정
    - 로그인 일부 수정
    - CSS 수정
    - 서버 통신 확인 완료

--- front_Nanal/src/config/Axios.js
+++ front_Nanal/src/config/Axios.js
@@ -1,11 +1,8 @@
 import axios, { AxiosInstance } from "axios";
-import cookies from "js-cookie";
 
 const axios_api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-  headers: {
-    access_token: cookies.get("access_token"),
-  },
+  headers: {},
 });
 
 export default axios_api;
