9a66249c25c4e23bbab7ac5e9f61af995e2e15c7
박소연 <soyeon_9605@yu.ac.kr>
2023-01-29 02:26:34 +0900

    Modify : login jwt
    - 서버와 통신 전 코드 추후 수정 예정
    - recoil 미사용
    - httponly, secure cookie 고려
    - axios header 고려

--- front_Nanal/src/index.js
+++ front_Nanal/src/index.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import axios_api from './config/Axios';
 
-const rootElement = document.getElementById("root");
+axios_api.defaults.withCredentials = true;
+
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
   <BrowserRouter>
