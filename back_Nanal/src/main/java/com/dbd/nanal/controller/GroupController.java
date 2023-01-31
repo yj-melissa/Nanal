23aef3119763977c55d50f74993773fbd2851b86
yujung lim <melimelame@gmail.com>
2023-01-31 17:46:46 +0900

    MODIFY : Refresh Token 쿠키로 발급 - 로그인, 회원가입 제외 전체 로그인해야 접근 가능하도록 수정

--- back_Nanal/src/main/java/com/dbd/nanal/controller/GroupController.java
+++ back_Nanal/src/main/java/com/dbd/nanal/controller/GroupController.java
@@ -158,18 +158,13 @@ public class GroupController {
                     "[Back] \n" +
                     "JSON\n" +
                     "{List<GroupDetailResponse>}")
-    @GetMapping("/list/{userIdx}")
-//    @GetMapping("/list")
-    public ResponseEntity<?> getGroupList(@ApiParam(value = "유저 idx", required = true) @PathVariable int userIdx) {
-//    public ResponseEntity<?> getGroupList(@ApiParam(value = "유저 idx", required = true) @AuthenticationPrincipal UserEntity userInfo) {
+    @GetMapping("/list")
+    public ResponseEntity<?> getGroupList(@ApiParam(value = "유저 idx", required = true) @AuthenticationPrincipal UserEntity userInfo) {
         HashMap<String, Object> responseDTO = new HashMap<>();
 
         try {
-            // group_user_relation 테이블에서 userIdx가 포함된 group찾기
             List<HashMap<String, Object>> groupDetailResponseDTOS =
-                    groupService.getGroupList(userIdx);
-//            List<HashMap<String, Object>> groupDetailResponseDTOS =
-//                    groupService.getGroupList(userInfo.getUserIdx());
+                    groupService.getGroupList(userInfo.getUserIdx());
 
             System.out.println("list : " + groupDetailResponseDTOS.size());
 
