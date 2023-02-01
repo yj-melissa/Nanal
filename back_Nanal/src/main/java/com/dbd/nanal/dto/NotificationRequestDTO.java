bd0c91df094e4e4f14465834ae5f5b3c39d9fc10
박소희 <psh990525@naver.com>
2023-02-01 16:20:29 +0900

    Feat : 그룹 가입, 새 글, 댓글 notice

--- back_Nanal/src/main/java/com/dbd/nanal/dto/NotificationRequestDTO.java
+++ back_Nanal/src/main/java/com/dbd/nanal/dto/NotificationRequestDTO.java
@@ -1,10 +1,14 @@
 package com.dbd.nanal.dto;
 
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 
 @Getter
+@Setter
 public class NotificationRequestDTO {
-    private int userIdx;
+    private List<Integer> userIdx;
     private int request_user_idx;
     private int request_group_idx;
     private int notice_type;
