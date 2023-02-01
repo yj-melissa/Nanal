94faad94faad4a370a6901ed143a3b1a84e175c2
recorror <itgwd9701@gmail.com>
2023-02-01 18:03:11 +0900

    Feat : setting 추가, profile 수정 기능 추가
    - 거의 다 끝냄!!

--- front_Nanal/src/components/mypage/Settings.js
+++ front_Nanal/src/components/mypage/Settings.js
@@ -2,22 +2,38 @@ import { Link } from 'react-router-dom';
 import bookmarksRed from '../../src_assets/img/bookmarksRed.png';
 import groupYellow from '../../src_assets/img/groupYellow.png';
 import recycleBinImg from '../../src_assets/img/recycleBin.png';
+import logOutImg from '../../src_assets/img/log-out.svg';
+import settingImg from '../../src_assets/img/cog.svg';
 
 function Settings() {
   return (
-    <div className='grid grid-cols-3 gap-6'>
-      <Link to='/Group/List' className='grid content-evenly'>
-        <img src={bookmarksRed} className='m-auto' />
-        <p className='text-center my-1'>그룹 관리</p>
-      </Link>
-      <Link to='/Friend/List' className='grid content-evenly'>
-        <img src={groupYellow} className='m-auto' />
-        <p className='text-center my-1'>친구 목록</p>
-      </Link>
-      <Link to='/RecycleBin' className='grid content-evenly'>
-        <img src={recycleBinImg} className='m-auto' />
-        <p className='text-center my-1'>휴지통</p>
-      </Link>
+    <div>
+      <div className='grid grid-cols-3 gap-6'>
+        <Link to='/Group/List' className='grid content-evenly'>
+          <img src={bookmarksRed} className='m-auto' />
+          <p className='text-center my-1'>그룹 관리</p>
+        </Link>
+        <Link to='/Friend/List' className='grid content-evenly'>
+          <img src={groupYellow} className='m-auto' />
+          <p className='text-center my-1'>친구 목록</p>
+        </Link>
+        <Link to='/RecycleBin' className='grid content-evenly'>
+          <img src={recycleBinImg} className='m-auto' />
+          <p className='text-center my-1'>휴지통</p>
+        </Link>
+      </div>
+      <br />
+      <br />
+      <div className='grid grid-cols-2'>
+        <Link to='/Group/List' className='grid content-evenly pl-4'>
+          <img src={logOutImg} className='w-10 h-10 m-auto' />
+          <p className='text-center my-1 ml-3'>로그아웃</p>
+        </Link>
+        <Link to='/Friend/List' className='grid content-evenly pr-10'>
+          <img src={settingImg} className='w-10 h-10 m-auto' />
+          <p className='text-center my-1'>설정</p>
+        </Link>
+      </div>
     </div>
   );
 }
