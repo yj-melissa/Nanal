b44e10b769a10d84a61dd309a0a60c5512ec468f
Awake-Ukey <hn99515@gmail.com>
2023-02-01 22:22:49 +0900

    Modfiy : Bookmark 기능#2

--- front_Nanal/src/components/diary/BookmarkList.js
+++ front_Nanal/src/components/diary/BookmarkList.js
@@ -1,15 +1,16 @@
-import { useState, useEffect } from 'react';
-import axios_api from '../../config/Axios';
+import { useState, useEffect } from "react";
+import axios_api from "../../config/Axios";
+import BookmarkItem from "./BookmarkItem";
 
 function FavoriteList() {
   // 좋아하는 일기 데이터 받기
   const [favoriteDiary, setFavoriteDiary] = useState([]);
 
   useEffect(() => {
-    axios_api.get('diary/bookmark').then(({ data }) => {
+    axios_api.get("diary/bookmark").then(({ data }) => {
       if (data.statusCode === 200) {
         setFavoriteDiary(null);
-        if (data.data.responseMessage === '일기 북마크 리스트 조회 성공') {
+        if (data.data.responseMessage === "일기 북마크 리스트 조회 성공") {
           setFavoriteDiary(data.data.dairy);
         }
       } else {
@@ -17,17 +18,16 @@ function FavoriteList() {
         console.log(data.data.responseMessage);
       }
     });
-  }, []);
+  }, [favoriteDiary]);
 
   return (
     <div>
       <h4>좋아하는 일기 개수는 총 {favoriteDiary.length}개 입니다.</h4>
-      {/* <div>
+      <div>
         {favoriteDiary.map((pictures, idx) => (
-          <FavoriteItem key={idx} {...pictures} />
+          <BookmarkItem key={idx} {...pictures} />
         ))}
-      </div> */}
-      <div>그림 들어갈 자리</div>
+      </div>
     </div>
   );
 }
