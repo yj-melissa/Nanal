dbfef478e17e0943742b2d9aff1ff5a3ca66f492
hn99515 <hn99515@gmail.com>
2023-02-02 09:51:35 +0900

    Fix : Bookmark 리스트 조회

--- front_Nanal/src/components/diary/BookmarkList.js
+++ front_Nanal/src/components/diary/BookmarkList.js
@@ -1,24 +1,24 @@
-import { useState, useEffect } from "react";
-import axios_api from "../../config/Axios";
-import BookmarkItem from "./BookmarkItem";
+import { useState, useEffect } from 'react';
+import axios_api from '../../config/Axios';
+import BookmarkItem from './BookmarkItem';
 
 function FavoriteList() {
   // 좋아하는 일기 데이터 받기
   const [favoriteDiary, setFavoriteDiary] = useState([]);
 
   useEffect(() => {
-    axios_api.get("diary/bookmark").then(({ data }) => {
+    axios_api.get('diary/bookmark/list').then(({ data }) => {
       if (data.statusCode === 200) {
         setFavoriteDiary(null);
-        if (data.data.responseMessage === "일기 북마크 리스트 조회 성공") {
-          setFavoriteDiary(data.data.dairy);
+        if (data.data.responseMessage === '일기 북마크 리스트 조회 성공') {
+          setFavoriteDiary(data.data.BookmarkList);
         }
       } else {
         console.log(data.statusCode);
         console.log(data.data.responseMessage);
       }
     });
-  }, [favoriteDiary]);
+  }, []);
 
   return (
     <div>
