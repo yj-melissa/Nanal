4aba910204ba359dd58480493926739a643a1527
박소희 <psh990525@naver.com>
2023-02-03 16:23:47 +0900

    Modify : 알림 content 수정

--- back_Nanal/src/main/java/com/dbd/nanal/service/NoticeService.java
+++ back_Nanal/src/main/java/com/dbd/nanal/service/NoticeService.java
@@ -23,7 +23,7 @@ public class NoticeService {
     //save friend notice
     public void saveFriendNotice(NotificationRequestDTO notice){
         String nickname=userProfileRepository.getReferenceById(notice.getRequest_user_idx()).getNickname();
-        String str=nickname+"님이 친구 신청을 요청했습니다.";
+        String str="[친구 초대] "+ nickname+"님으로부터의 친구 등록 요청입니다.";
 
         for(int userIdx: notice.getUserIdx()) {
             NoticeEntity noticeEntity = NoticeEntity.builder()
@@ -40,7 +40,7 @@ public class NoticeService {
         String nickname=userProfileRepository.getReferenceById(notice.getRequest_user_idx()).getNickname();
         String groupName=groupRepository.getReferenceById(notice.getRequest_group_idx()).getGroupName();
 
-        String str=nickname+"님이 "+groupName+" 그룹 일기에 초대했습니다.";
+        String str="[그룹 초대] "+nickname+"님이 "+groupName+" 그룹 일기에 초대했습니다.";
 
         for(int userIdx: notice.getUserIdx()){
             NoticeEntity noticeEntity=NoticeEntity.builder()
@@ -56,12 +56,11 @@ public class NoticeService {
 
     //save comment notice
     public void saveCommentNotice(NotificationRequestDTO notice){
-        String nickname=userProfileRepository.getReferenceById(notice.getRequest_user_idx()).getNickname();
-        String groupName=groupRepository.getReferenceById(notice.getRequest_group_idx()).getGroupName();
         DiaryEntity diaryEntity=diaryRepository.getReferenceById(notice.getRequest_diary_idx());
-        String diaryDate=diaryEntity.getCreationDate().toString();
+        String userName=userProfileRepository.getReferenceById(diaryEntity.getUser().getUserIdx()).getNickname();
+        String nickname=userProfileRepository.getReferenceById(notice.getRequest_user_idx()).getNickname();
 
-        String str=nickname+"님이 "+groupName+"의 "+diaryDate+"일기에 새로운 댓글을 남겼습니다.";
+        String str="[댓글] "+ userName +"님의 일기에 "+ nickname+"님이 댓글을 작성하셨습니다.";
 
         NoticeEntity noticeEntity=NoticeEntity.builder()
                 .user(UserEntity.builder().userIdx(diaryEntity.getUser().getUserIdx()).build())
@@ -79,7 +78,7 @@ public class NoticeService {
         String groupName=groupRepository.getReferenceById(notice.getRequest_group_idx()).getGroupName();
         List<GroupUserRelationEntity> groupUserRelationEntityList=groupUserRelationRepository.findByGroupDetail(GroupDetailEntity.builder().groupIdx(notice.getRequest_group_idx()).build());
 
-        String str=nickname+"님이 "+groupName+" 그룹에 "+" 새 글을 등록하였습니다.";
+        String str="[새글] "+nickname+"님이 "+groupName+" 그룹에 새 글을 등록하였습니다.";
         for(GroupUserRelationEntity groupUser: groupUserRelationEntityList){
             NoticeEntity noticeEntity=NoticeEntity.builder()
                     .user(UserEntity.builder().userIdx(groupUser.getUser().getUserIdx()).build())
