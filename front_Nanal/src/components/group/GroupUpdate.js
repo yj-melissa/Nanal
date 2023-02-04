64a8b3edcf1d938870290ae6f4d5749a3a7910a6
박소연 <soyeon_9605@yu.ac.kr>
2023-02-05 05:17:19 +0900

    Modify : Group Update
    - group update 수정
    - 최적화 진행 중
    - 서버 통신 확인

--- front_Nanal/src/components/group/GroupUpdate.js
+++ front_Nanal/src/components/group/GroupUpdate.js
@@ -11,14 +11,25 @@ function GroupUpdate() {
   const [groupName, setGroupName] = useState('');
   const [groupTag, setGroupTag] = useState([]);
 
+  const currentName = useRef('');
   const currentTag = useRef([]);
-  console.log('hhiihhihi');
 
   const [groupFriendList, setGroupFriendList] = useState([]);
   const [groupNotFriendList, setGroupNotFriendList] = useState([]);
   const [includeFriend, setIncludeFriend] = useState([]);
   const [includeFriendIdx, setIncludeFriendIdx] = useState([]);
-  let hi = 0;
+
+  // const currentFriend = useRef([{}]);
+
+  // tag 변경 시 요청되는 함수
+  const updateTag = (e, idx) => {
+    const newTag = e.target.value;
+    currentTag.current[idx].tag = newTag;
+
+    // setGroupTag(
+    //   groupTag.map((it) => (it.tagIdx === idx ? { ...it, tag: newTag } : it))
+    // );
+  };
 
   // 초대할 사용자 추가
   const addFriend = (idx) => {
@@ -61,57 +72,57 @@ function GroupUpdate() {
     } else if (isCurrentGTName === false) {
       alert('태그명은 10글자 이하로 입력해주세요!');
     } else {
-      console.log(groupIdx);
-      console.log(groupName);
-      console.log(groupTag);
-      console.log(currentTag);
-      console.log(includeFriend);
-      // axios_api
-      //   .put('/group', {
-      //     groupIdx: groupIdx,
-      //     groupName: groupName,
-      //     tags: groupTag,
-      //   })
-      //   .then(({ data }) => {
-      //     if (data.statusCode === 200) {
-      //       if (data.data.responseMessage === '그룹 수정 성공') {
-      //         // console.log(data.data.groupDetail);
-      //         // console.log(data.data.tags);
+      // setGroupName(currentName.current);
+      // setGroupTag(currentTag.current);
 
-      //         const groupidx = data.data.groupDetail.groupIdx;
+      axios_api
+        .put('/group', {
+          groupIdx: groupIdx,
+          // groupName: groupName,
+          // tags: groupTag,
+          groupName: currentName,
+          tags: currentTag,
+        })
+        .then(({ data }) => {
+          if (data.statusCode === 200) {
+            if (data.data.responseMessage === '그룹 수정 성공') {
+              console.log(data.data.groupDetail);
+              console.log(data.data.tags);
 
-      //         if (includeFriend.size !== 0) {
-      //           // 그룹에 추가할 친구가 있는 경우
-      //           axios_api
-      //             .post('notification/group', {
-      //               request_group_idx: groupidx,
-      //               userIdx: includeFriendIdx,
-      //             })
-      //             .then(({ data }) => {
-      //               if (data.statusCode === 200) {
-      //                 if (data.data.responseMessage === '알림 저장 성공') {
-      //                   window.location.replace('/Group/List');
-      //                 }
-      //               } else {
-      //                 console.log('알림 저장 오류 : ');
-      //                 console.log(data.statusCode);
-      //                 console.log(data.data.responseMessage);
-      //               }
-      //             })
-      //             .catch(({ error }) => {
-      //               console.log('알림 저장 오류 : ' + error);
-      //             });
-      //         }
-      //       }
-      //     } else {
-      //       console.log('그룹 수정 오류: ');
-      //       console.log(data.statusCode);
-      //       console.log(data.data.responseMessage);
-      //     }
-      //   })
-      //   .catch(({ error }) => {
-      //     console.log('그룹 수정 성공: ' + error);
-      //   });
+              const groupidx = data.data.groupDetail.groupIdx;
+
+              if (includeFriend.size !== 0) {
+                // 그룹에 추가할 친구가 있는 경우
+                axios_api
+                  .post('notification/group', {
+                    request_group_idx: groupidx,
+                    userIdx: includeFriendIdx,
+                  })
+                  .then(({ data }) => {
+                    if (data.statusCode === 200) {
+                      if (data.data.responseMessage === '알림 저장 성공') {
+                        window.location.replace('/Group/List');
+                      }
+                    } else {
+                      console.log('알림 저장 오류 : ');
+                      console.log(data.statusCode);
+                      console.log(data.data.responseMessage);
+                    }
+                  })
+                  .catch(({ error }) => {
+                    console.log('알림 저장 오류 : ' + error);
+                  });
+              }
+            }
+          } else {
+            console.log('그룹 수정 오류: ');
+            console.log(data.statusCode);
+            console.log(data.data.responseMessage);
+          }
+        })
+        .catch(({ error }) => {
+          console.log('그룹 수정 성공: ' + error);
+        });
     }
   };
 
@@ -124,10 +135,13 @@ function GroupUpdate() {
           setGroupName(null);
           setGroupTag(null);
           if (data.data.responseMessage === '그룹 조회 성공') {
+            // console.log(data.data.groupDetail);
             setGroupIdx(data.data.groupDetail.groupIdx);
             setGroupName(data.data.groupDetail.groupName);
             setGroupTag(data.data.tags);
+
             const groupidx = data.data.groupDetail.groupIdx;
+            currentName.current = data.data.groupDetail.groupName;
             currentTag.current = data.data.tags;
 
             axios_api
@@ -204,43 +218,25 @@ function GroupUpdate() {
               type='text'
               id='group-name'
               className='font-bold m-0.5'
-              defaultValue={groupName || ''}
-              onChange={(e) => setGroupName(e.target.value)}
-              autoFocus
+              defaultValue={currentName.current || ''}
+              onChange={(e) => (currentName.current = e.target.value)}
             ></input>
           </div>
           <div id='group-tag-div'>
-            <input
-              type='text'
-              className='mr-2 mb-2'
-              defaultValue={groupTag[0].tag || ''}
-              onChange={(e) => console.log(e.target.value)}
-            ></input>
-            <input
-              type='text'
-              className='mr-2 mb-2'
-              defaultValue={groupTag[1].tag || ''}
-              onChange={(e) => console.log(e.target.value)}
-            ></input>
-            <input
-              type='text'
-              className='mr-2 mb-2'
-              defaultValue={groupTag[2].tag || ''}
-              onChange={(e) => console.log(e.target.value)}
-            ></input>
-            <input
-              type='text'
-              className='mr-2 mb-2'
-              defaultValue={groupTag[3].tag || ''}
-              onChange={(e) => console.log(e.target.value)}
-            ></input>
-            <input
-              type='text'
-              className='mr-2 mb-2'
-              defaultValue={groupTag[4].tag || ''}
-              onChange={(e) => console.log(e.target.value)}
-            ></input>
-            {/* {groupTag.map((tagging, idx) => {
+            {/* {groupTag.map((tagging) => {
+              return (
+                <input
+                  type='text'
+                  className='mr-2 mb-2'
+                  key={tagging.tagIdx}
+                  defaultValue={tagging.tag || ''}
+                  onChange={(e) => {
+                    updateTag(e, tagging.tagIdx);
+                  }}
+                ></input>
+              );
+            })} */}
+            {currentTag.current.map((tagging, idx) => {
               return (
                 <input
                   type='text'
@@ -248,11 +244,11 @@ function GroupUpdate() {
                   key={idx}
                   defaultValue={tagging.tag || ''}
                   onChange={(e) => {
-                    console.log(e.target);
+                    updateTag(e, idx);
                   }}
                 ></input>
               );
-            })} */}
+            })}
           </div>
           <div>
             {groupFriendList.map((friendItem, idx) => {
