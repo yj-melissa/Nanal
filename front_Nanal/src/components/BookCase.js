7ef7e2ae6850305d7eb88939c7164e3f9f3cbfd3
박소연 <soyeon_9605@yu.ac.kr>
2023-02-06 10:38:27 +0900

    Modify : Main Page Css
    - Main Page 변경
    - Csss 변경

--- front_Nanal/src/components/BookCase.js
+++ front_Nanal/src/components/BookCase.js
@@ -6,6 +6,8 @@ import emb from '../src_assets/img/emo_emb.png';
 import joy from '../src_assets/img/emo_joy.png';
 import nerv from '../src_assets/img/emo_nerv.png';
 import sad from '../src_assets/img/emo_sad.png';
+import addIcon from '../src_assets/img/file_add_icon.png';
+import { Link } from 'react-router-dom';
 
 function BookCase() {
   const [Data, setData] = useState([
@@ -21,26 +23,26 @@ function BookCase() {
     Collocate === false
       ? 'animate-pulse'
       : 'animate-bounce hover:animate-none top-24 left-12';
-  const nervCss =
-    Collocate === false
-      ? 'animate-pulse'
-      : 'animate-bounce hover:animate-none top-28 right-12';
   const calmCss =
     Collocate === false
       ? 'animate-pulse'
-      : 'animate-bounce hover:animate-none top-88 right-24';
-  const sadCss =
+      : 'animate-bounce hover:animate-none top-28 right-12';
+  const nervCss =
     Collocate === false
       ? 'animate-pulse'
       : 'animate-bounce hover:animate-none top-64 right-8';
   const angCss =
     Collocate === false
       ? 'animate-pulse'
-      : 'animate-bounce hover:animate-none top-88 left-24';
+      : 'animate-bounce hover:animate-none top-80 left-24';
   const embCss =
     Collocate === false
       ? 'animate-pulse'
-      : 'animate-bounce hover:animate-none top-80 left-24';
+      : 'animate-bounce hover:animate-none top-88 left-24';
+  const sadCss =
+    Collocate === false
+      ? 'animate-pulse'
+      : 'animate-bounce hover:animate-none top-88 right-24';
   return (
     <div>
       {/* bookshelf 마진처리 해야함!!! */}
@@ -70,19 +72,23 @@ function BookCase() {
           <div className='grid items-center h-16 w-16 relative box-border border-black text-center border-2'>
             그룹4
           </div>
-          <div className='grid items-center h-16 w-16 relative box-border border-black text-center border-2'>
-            그룹추가
-          </div>
+
+          <Link
+            to='/Group/List'
+            className='grid items-center h-12 w-10 relative text-center'
+          >
+            <img src={addIcon} className='m-auto mt-2' />
+          </Link>
         </div>
         {/* 책장 */}
         <img src={shelf} className='mx-auto py-3 w-60' />
       </div>
       <div className='flex justify-center'>
-        <img src={calm} className={`${emoCss} ${calmCss}`} />
-        <img src={emb} className={`${emoCss} ${embCss}`} />
-        <img src={nerv} className={`${emoCss} ${nervCss}`} />
         <img src={sad} className={`${emoCss} ${sadCss}`} />
+        <img src={nerv} className={`${emoCss} ${nervCss}`} />
+        <img src={emb} className={`${emoCss} ${embCss}`} />
         <img src={ang} className={`${emoCss} ${angCss}`} />
+        <img src={calm} className={`${emoCss} ${calmCss}`} />
         <img src={joy} className={`${emoCss} ${joyCss}`} />
         <button
           className='text-[12px] box-border rounded-full bg-slate-100/50 h-16 w-16 absolute'
