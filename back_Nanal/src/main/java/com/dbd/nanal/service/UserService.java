60e55f6051047e21bbce252265c71af10af3e4e4
임지원 <wldnjs4255@naver.com>
2023-02-07 11:08:55 +0900

    Feat : group 이미지 update

--- back_Nanal/src/main/java/com/dbd/nanal/service/UserService.java
+++ back_Nanal/src/main/java/com/dbd/nanal/service/UserService.java
@@ -179,7 +179,7 @@ public class UserService {
 
 
     @Transactional
-    public void updateUserImg(int userIdx, String newImgUrl) {
+    public void updateUserImg(int userIdx, int pictureIdx, String newImgUrl) {
         System.out.println("useridx : "+userIdx+ " newImgUrl : "+newImgUrl);
         UserProfileEntity userProfileEntity = userProfileRepository.getReferenceById(userIdx);
         System.out.println("userIdx : "+userIdx+" 변경 전 img idx : "+userProfileEntity.getImg()+" 변경 후 img idx : "+newImgUrl);
