4fc29084a1a2005146d9dbd60fccf2b22a089c18
recorror <itgwd9701@gamil.com>
2023-02-11 07:51:02 +0900

    Feat : web#2 폼, 세팅 폼

--- front_Nanal/src/webComponents/RecycleBin.js
+++ front_Nanal/src/webComponents/RecycleBin.js
@@ -1,13 +1,14 @@
-import nmg from '../src_assets/img/bookmark-name/name-mark-green.svg'
-import diaryImgGreen from '../src_assets/img/diary-img-green.svg'
+import nmb from '../src_assets/img/bookmark-name/name-mark-blue.svg'
+import diaryImgBlue from '../src_assets/img/diary-img-blue.svg'
 import React, { useState } from 'react';
 
 const RecycleBin = () => {
   return <div className="relative w-[1440px] mx-auto">
 
     <p className="absolute z-30 left-[330px] inset-y-28">휴지통</p>
-    <img src={nmg} className='absolute z-20 left-60 inset-y-20'/>
-    <img src={diaryImgGreen} className='absolute w-[1440px] z-10' />
+    <img src={nmb} className='absolute z-20 left-60 inset-y-20'/>
+    <img src={diaryImgBlue} className='absolute w-[1440px] z-10' />
+    
     <div>
       
     </div>
