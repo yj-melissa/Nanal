1d4eb01a51d3ee2b42dc1f06606b32b37919d011
recorror <itgwd9701@gamil.com>
2023-02-12 03:09:07 +0900

    Feat : webNav#2

--- front_Nanal/src/webComponents/LogoHome.js
+++ front_Nanal/src/webComponents/LogoHome.js
@@ -18,17 +18,17 @@ const LogoHome = () => {
   }, []);
 
   return (
-    <div className='grid grid-cols-1 mt-20 place-items-center'>
-      <img src={nanal} alt='logo' className='w-96 h-96' />
-      <div className='my-4'>
-        <Link to='/SignIn'>
-          <button className='px-4 py-2 mx-10 text-base font-semibold border-0 rounded-full bg-violet-100 text-violet-500 hover:bg-violet-200'>
-            로그인
+    <div className='grid grid-cols-2 mt-20 place-items-center'>
+      <img src={nanal} alt='logo' className='w-96 h-96 border border-slate-800 rounded-lg' />
+      <div className='my-4 grid grid-cols-1'>
+        <Link to='/SignIn' className="my-3">
+          <button className='py-2 w-72 text-base font-semibold border border-violet-900 rounded-full bg-violet-100 text-violet-500 hover:bg-violet-200'>
+            로그인하기
           </button>
         </Link>
-        <Link to='/SignUp'>
-          <button className='px-4 py-2 mx-10 text-base font-semibold border-0 rounded-full bg-violet-100 text-violet-500 hover:bg-violet-200'>
-            회원가입
+        <Link to='/SignUp' className="my-3">
+          <button className='py-2 w-72 text-base font-semibold border border-violet-900 rounded-full bg-violet-100 text-violet-500 hover:bg-violet-200'>
+            회원가입하기
           </button>
         </Link>
       </div>
