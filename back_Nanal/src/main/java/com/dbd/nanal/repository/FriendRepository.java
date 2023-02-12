9256148c9a48aadb2d563f05e78afd8fc6d6e2c9
임지원 <wldnjs4255@naver.com>
2023-02-12 20:49:55 +0900

    Feat : 감정 통계 조회

--- back_Nanal/src/main/java/com/dbd/nanal/repository/FriendRepository.java
+++ back_Nanal/src/main/java/com/dbd/nanal/repository/FriendRepository.java
@@ -20,7 +20,7 @@ public interface FriendRepository extends JpaRepository<FriendEntity, Integer> {
     @Query("select f from FriendEntity f where f.user_idx1.userIdx = :userIdx and f.user_idx2.userIdx = :friendIdx")
     FriendEntity isFriendExist(@Param("userIdx") int userIdx, @Param("friendIdx") int friendIdx);
 
-    @Query(nativeQuery = true, value = "select distinct substring_index(substring_index(substring_index(d.emo,'/',-1),'_',2),'_',-1) as emo, d.user_idx, p.nickname from diary d join user_profile p on d.user_idx = p.user_idx where d.user_idx in (select f.user_idx2 from friend f where user_idx = 5) and d.diary_date= DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 1 DAY),'%Y-%m-%d')")
-    List<Object[]> findEmotionOfLastDay(int userIdx);
+    @Query(nativeQuery = true, value = "select distinct substring_index(substring_index(substring_index(d.emo,'/',-1),'_',2),'_',-1) as emo, p.nickname from diary d join user_profile p on d.user_idx = p.user_idx where d.user_idx in (select f.user_idx2 from friend f where user_idx = :userIdx) and d.diary_date= DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 1 DAY),'%Y-%m-%d')")
+    List<Object[]> findEmotionOfLastDay(@Param("userIdx") int userIdx);
 }
 
