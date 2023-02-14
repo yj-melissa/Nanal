2458964750a0b0b8221ad124f0dfe578182c7dd3
yujung lim <melimelame@gmail.com>
2023-02-14 10:04:21 +0900

    Modify : 유저 컨트롤러, 서비스 중복 코드 삭제

--- back_Nanal/src/main/java/com/dbd/nanal/controller/UserController.java
+++ back_Nanal/src/main/java/com/dbd/nanal/controller/UserController.java
@@ -58,21 +58,14 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody @Valid UserFormDTO userformDTO, HttpServletResponse response) {
         HashMap<String, Object> responseDTO = new HashMap<>();
-        // 정보가 들어오지 않았을 때
         if (userformDTO == null || userformDTO.getPassword() == null || userformDTO.getUserId() == null | userformDTO.getEmail() == null || userformDTO.getNickname() == null) {
+            // 정보가 들어오지 않았을 때
             responseDTO.put("responseMessage", ResponseMessage.USER_CREATE_FAIL);
-
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
-        }
-
-        // 중복값 체크
-        if(userService.checkUserId(userformDTO.getUserId()) || userService.checkEmail(userformDTO.getEmail()) || userService.checkNickname(userformDTO.getNickname())) {
+        } else if (userService.checkUserId(userformDTO.getUserId()) || userService.checkEmail(userformDTO.getEmail()) || userService.checkNickname(userformDTO.getNickname())) {
+            // 중복값이 있을 때
             responseDTO.put("responseMessage", ResponseMessage.DUPLICATED_KEY);
-
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
-        }
-
-        UserEntity user = UserEntity.builder()
+        } else {
+            UserEntity user = UserEntity.builder()
                 .userId(userformDTO.getUserId())
                 .name(userformDTO.getName())
                 .email(userformDTO.getEmail())
@@ -82,33 +75,33 @@ public class UserController {
                 .lastAccessDate(LocalDateTime.now())
                 .build();
 
-        PaintingRequestDTO paintingRequestDTO = new PaintingRequestDTO();
-        paintingRequestDTO.init();
-        fileService.paintingSave(paintingRequestDTO);
+            PaintingRequestDTO paintingRequestDTO = new PaintingRequestDTO();
+            paintingRequestDTO.init();
+            fileService.paintingSave(paintingRequestDTO);
 
-        UserProfileEntity userProfile = UserProfileEntity.builder()
+            UserProfileEntity userProfile = UserProfileEntity.builder()
                 .user(user)
                 .nickname(userformDTO.getNickname())
-//                .img(userformDTO.getImg())
                 .introduction(userformDTO.getIntroduction())
                 .isPrivate(userformDTO.getIsPrivate())
                 .painting(paintingRequestDTO.toEntity())
                 .img(paintingRequestDTO.getImgUrl())
                 .build();
 
-        UserEntity createdUser = userService.join(user, userProfile);
-        log.debug("createdUser : " + createdUser);
+            UserEntity createdUser = userService.join(user, userProfile);
+            log.debug("createdUser : " + createdUser);
 
 //          JWT 토큰 발행
-        HashMap<String, String> token = createTokens(user);
+            HashMap<String, String> token = createTokens(user);
 
-        responseDTO.put("responseMessage", ResponseMessage.USER_CREATE_SUCCESS);
-        responseDTO.put("accessToken", token.get("accessToken"));
-        response.setHeader("accessToken", token.get("accessToken"));
+            responseDTO.put("responseMessage", ResponseMessage.USER_CREATE_SUCCESS);
+            responseDTO.put("accessToken", token.get("accessToken"));
+            response.setHeader("accessToken", token.get("accessToken"));
 
-        Cookie refreshTokenCookie = refreshTokenCookie(token.get("refreshToken"));
+            Cookie refreshTokenCookie = refreshTokenCookie(token.get("refreshToken"));
 
-        response.addCookie(refreshTokenCookie);
+            response.addCookie(refreshTokenCookie);
+        }
 
         return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
@@ -125,15 +118,15 @@ public class UserController {
 
         // 이메일 중복 확인
         Boolean isDuplicate = userService.checkEmail(email);
+
         if (isDuplicate) {
             responseDTO.put("responseMessage", ResponseMessage.DUPLICATED_KEY);
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
+        } else {
+            String code = emailService.sendSimpleMessage(email);
+
+            responseDTO.put("responseMessage", ResponseMessage.EMAIL_SEND_SUCCESS);
+            responseDTO.put("code", code);
         }
-
-        String code = emailService.sendSimpleMessage(email);
-
-        responseDTO.put("responseMessage", ResponseMessage.EMAIL_SEND_SUCCESS);
-        responseDTO.put("code", code);
 
         return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
@@ -152,11 +145,12 @@ public class UserController {
                 userRequestDTO.getPassword(),
                 passwordEncoder);
 
+        HashMap<String, Object> responseDTO = new HashMap<>();
+
         if (user != null) {
             // 로그인 성공 -> JWT 발급
             HashMap<String, String> token = createTokens(user);
 
-            HashMap<String, Object> responseDTO = new HashMap<>();
             responseDTO.put("responseMessage", ResponseMessage.LOGIN_SUCCESS);
             responseDTO.put("token", token);
             response.setHeader("accessToken", token.get("accessToken"));
@@ -164,13 +158,9 @@ public class UserController {
             Cookie refreshTokenCookie = refreshTokenCookie(token.get("refreshToken"));
 
             response.addCookie(refreshTokenCookie);
-
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
-
         } else {
             // 로그인 실패
             Boolean isUserExist = userService.isUserExist(userRequestDTO.getUserId());
-            HashMap<String, Object> responseDTO = new HashMap<>();
 
             if (isUserExist) {
                 // 비밀번호 미일치
@@ -179,8 +169,8 @@ public class UserController {
                 // 해당 아이디 없음
                 responseDTO.put("responseMessage", ResponseMessage.USER_FIND_FAIL);
             }
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
         }
+        return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
 
     @ApiOperation(value = "로그아웃", notes =
@@ -207,12 +197,11 @@ public class UserController {
         if (newToken == null) {
             response.sendRedirect("/nanal/user/login");
             responseDTO.put("responseMessage", ResponseMessage.TOKEN_NOT_VALID);
-            return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+        } else {
+            responseDTO.put("responseMessage", ResponseMessage.SUCCESS);
+            responseDTO.put("accessToken", newToken);
+            response.setHeader("accessToken", newToken);
         }
-
-        responseDTO.put("responseMessage", ResponseMessage.SUCCESS);
-        responseDTO.put("accessToken", newToken);
-        response.setHeader("accessToken", newToken);
 
         return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
@@ -259,18 +248,17 @@ public class UserController {
     public ResponseEntity<?> updateProfile(@AuthenticationPrincipal UserEntity userInfo, @RequestBody UserRequestDTO userRequest) {
         log.info("updateProfile 실행 : {}", userInfo.getUserProfile().getNickname());
 
+        HashMap<String, Object> responseDTO = new HashMap<>();
+
         if (userRequest == null) {
             log.info("updateProfile : userRequest == null");
-            HashMap<String, Object> responseDTO = new HashMap<>();
             responseDTO.put("responseMessage", ResponseMessage.USER_UPDATE_FAIL);
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
+        } else {
+            HashMap<String, Object> profile = userService.updateProfile(userInfo.getUserIdx(), userRequest);
+
+            responseDTO.put("responseMessage", ResponseMessage.USER_UPDATE_SUCCESS);
+            responseDTO.put("profile", profile);
         }
-
-        HashMap<String, Object> profile = userService.updateProfile(userInfo.getUserIdx(), userRequest);
-
-        HashMap<String, Object> responseDTO = new HashMap<>();
-        responseDTO.put("responseMessage", ResponseMessage.USER_UPDATE_SUCCESS);
-        responseDTO.put("profile", profile);
 
         return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
@@ -285,7 +273,7 @@ public class UserController {
     public ResponseEntity<?> deleteUser(@AuthenticationPrincipal UserEntity userInfo) {
         log.info("deleteUser 실행");
         int userIdx = userInfo.getUserIdx();
-        userService.deleteByUserIdx(userIdx);
+        userService.deleteByUserIdx(userInfo.getUserIdx());
         jwtTokenProvider.deleteRefreshToken(userIdx);
         HashMap<String, Object> responseDTO = new HashMap<>();
         responseDTO.put("responseMessage", ResponseMessage.USER_DELETE_SUCCESS);
@@ -310,12 +298,10 @@ public class UserController {
 
         if (isCorrect) {
             responseDTO.put("responseMessage", ResponseMessage.LOGIN_SUCCESS);
-
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
         } else {
             responseDTO.put("responseMessage", ResponseMessage.LOGIN_FAIL);
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
         }
+        return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
 
     @ApiOperation(value = "비밀번호 수정", notes =
@@ -330,14 +316,13 @@ public class UserController {
         HashMap<String, Object> responseDTO = new HashMap<>();
         if (userRequestDTO.getPassword() == null) {
             responseDTO.put("responseMessage", ResponseMessage.EMPTY);
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
+        } else {
+            String newPassword = passwordEncoder.encode(userRequestDTO.getPassword());
+
+            userService.updatePassword(userInfo.getUserIdx(), newPassword);
+
+            responseDTO.put("responseMessage", ResponseMessage.PASSWORD_UPDATE_SUCEESS);
         }
-
-        String newPassword = passwordEncoder.encode(userRequestDTO.getPassword());
-
-        userService.updatePassword(userInfo.getUserIdx(), newPassword);
-
-        responseDTO.put("responseMessage", ResponseMessage.PASSWORD_UPDATE_SUCEESS);
 
         return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
@@ -365,14 +350,14 @@ public class UserController {
                     "{responseMessage(String)}")
     @GetMapping("/check/id/{userId}")
     public ResponseEntity<?> checkUserId(@PathVariable String userId) {
+        HashMap<String, Object> responseDTO = new HashMap<>();
+
         if (userService.checkUserId(userId)) {
-            HashMap<String, Object> responseDTO = new HashMap<>();
             responseDTO.put("responseMessage", ResponseMessage.DUPLICATED_KEY);
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
+        } else {
+            responseDTO.put("responseMessage", ResponseMessage.USUABLE_KEY);
         }
 
-        HashMap<String, Object> responseDTO = new HashMap<>();
-        responseDTO.put("responseMessage", ResponseMessage.USUABLE_KEY);
         return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
 
@@ -383,14 +368,14 @@ public class UserController {
                     "{responseMessage(String)}")
     @GetMapping("/check/nickname/{nickname}")
     public ResponseEntity<?> checkNickname(@PathVariable String nickname) {
+        HashMap<String, Object> responseDTO = new HashMap<>();
+
         if (userService.checkNickname(nickname)) {
-            HashMap<String, Object> responseDTO = new HashMap<>();
             responseDTO.put("responseMessage", ResponseMessage.DUPLICATED_KEY);
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
+        } else {
+            responseDTO.put("responseMessage", ResponseMessage.USUABLE_KEY);
         }
 
-        HashMap<String, Object> responseDTO = new HashMap<>();
-        responseDTO.put("responseMessage", ResponseMessage.USUABLE_KEY);
         return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
 
@@ -401,14 +386,13 @@ public class UserController {
                     "{responseMessage(String)}")
     @GetMapping("/check/Email/{email}")
     public ResponseEntity<?> checkEmail(@PathVariable String email) {
-        if (userService.checkEmail(email)) {
-            HashMap<String, Object> responseDTO = new HashMap<>();
-            responseDTO.put("responseMessage", ResponseMessage.DUPLICATED_KEY);
-            return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
-        }
-
         HashMap<String, Object> responseDTO = new HashMap<>();
-        responseDTO.put("responseMessage", ResponseMessage.USUABLE_KEY);
+
+        if (userService.checkEmail(email)) {
+            responseDTO.put("responseMessage", ResponseMessage.DUPLICATED_KEY);
+        } else {
+            responseDTO.put("responseMessage", ResponseMessage.USUABLE_KEY);
+        }
         return new ResponseEntity<>(DefaultRes.res(200, responseDTO), HttpStatus.OK);
     }
 
