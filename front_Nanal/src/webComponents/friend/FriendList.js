2b63a766785145a2a015caa15509c6ac86a704cc
hn99515 <hn99515@gmail.com>
2023-02-15 16:40:35 +0900

    Design : Web 디자인 수정

--- front_Nanal/src/webComponents/friend/FriendList.js
+++ front_Nanal/src/webComponents/friend/FriendList.js
@@ -3,26 +3,9 @@ import axios_api from '../../config/Axios';
 import { onLogin } from '../../config/Login';
 import FriendItem from './FriendItem';
 import addIcon from '../../src_assets/img/user_add_icon.png';
-import styled from 'styled-components';
-
-const Div = styled.div`
-  overflow: scroll;
-  &::-webkit-scrollbar {
-    width: 8px;
-    height: 8px;
-    border-radius: 6px;
-    background: rgba(255, 255, 255, 0.1);
-  }
-  &::-webkit-scrollbar-thumb {
-    background: rgba(0, 0, 0, 0.3);
-    border-radius: 6px;
-  }
-`;
 
 function FriendList({ setFriendAdd, setUserIdx }) {
-  // console.log(setFriendCompo)
   const [friendList, setFriendList] = useState([]);
-  // console.log(friendList)
 
   useEffect(() => {
     onLogin();
@@ -48,14 +31,18 @@ function FriendList({ setFriendAdd, setUserIdx }) {
   }, []);
 
   return (
-    <Div className='overflow-auto w-80 h-80'>
+    <div className='overflow-auto w-80 h-80'>
       <div id='friend-list-title' className='mb-2'>
         <h1 className='inline m-1 ml-2 font-bold'>친구 리스트 조회</h1>
         <div
           className='inline-block float-right'
           onClick={() => setFriendAdd([false, true, false])}
         >
-          <img src={addIcon} className='w-[20px] h-[20px] mx-1.5' alt='add-icon'/>
+          <img
+            src={addIcon}
+            className='w-[20px] h-[20px] mx-1.5'
+            alt='add-icon'
+          />
         </div>
       </div>
       {friendList.length !== 0 ? (
@@ -70,7 +57,7 @@ function FriendList({ setFriendAdd, setUserIdx }) {
       ) : (
         <p>아직은 친구가 없습니다.</p>
       )}
-    </Div>
+    </div>
   );
 }
 
